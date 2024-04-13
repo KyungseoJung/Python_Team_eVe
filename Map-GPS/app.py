b12802f07b0dc010c2b230ba0dd7bff0d73f68bb
KyungseoJung <junggs1245@gmail.com>
2024-04-13 16:54:55 +0900

    docs(geo_location.html): Comment out unnecessary parts #5

--- Map-GPS/app.py
+++ Map-GPS/app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index_map_gps.html')
 
-# //#5 현재 내 위치를 GPS로 받아와서 TMAP 지도 시각화
-@app.route('/get_location') 
-def get_location():
-    return render_template('get_location.html')
+# # //#5 현재 내 위치를 GPS로 받아와서 TMAP 지도 시각화
+# @app.route('/get_location') 
+# def get_location():
+#     return render_template('get_location.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
