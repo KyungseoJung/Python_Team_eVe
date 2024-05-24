ef0d6a0bcf6bbfb4a6a74bde59b1c31d7e61b901
KyungseoJung <junggs1245@gmail.com>
2024-05-25 01:49:57 +0900

    feat(RabbitMQ-Administrator/file): Change page names and structure code with comments

--- RabbitMQ-Administrator/app_Administrator.py
+++ RabbitMQ-Administrator/app_Administrator.py
@@ -3,16 +3,18 @@
 # //#26 관리자(Administrator) 웹화면 디자인 구성 
 # //#27 관리자 웹페이지 디자인 
 # //#28 수리모형 코드 통합 - [경로 계산하기] 버튼 누르면, 수리모형 코드 실행되도록 - csv 파일 지정한 파일 위치에 저장되도록
+# //#29 배터리 강화학습 코드 통합
 
 from flask import Flask, render_template
 from flask import jsonify, request # //#28 수리모형 코드 통합을 위한 import
 
 import pandas as pd
-import eve_0522_test3 # //#28 수리모형 코드 통합 (Import 수리모형 함수를 포함한 Python file )
+import eve_0523_test1 # //#28 수리모형 코드 통합 (Import 수리모형 함수를 포함한 Python file )
 
 app = Flask(__name__)
 
 #===================================================================================================
+# //#29 배터리 강화학습 코드 통합 - 여기부터
 
 
 import pandas as pd
@@ -358,21 +360,25 @@ def battery_state(battery_index):
     state = get_battery_state(battery_index)
     return jsonify([state])
 
+# //#29 배터리 강화학습 코드 통합 - 여기까지
 #===================================================================================================
+
 
 @app.route('/calculate_path', methods=['POST'])
 def calculate_path():
         # import eve_0521_test
     try:
         # //#28 내가 불러올 데이터
-        datafile = "C:/GitStudy/Python_Team_eVe/RabbitMQ-Administrator/orderData/S_02.txt"
+        datafile = "C:/GitStudy/Python_Team_eVe/RabbitMQ-Administrator/orderData/S_03.txt"
 
         # //#28 내가 지정하는 경로 (파일 저장)
-        pickle_path = "C:/GitStudy/Python_Team_eVe/RabbitMQ-Administrator/static/all_k_shortest_paths.pickle_S_02"
+        # //#28 fix: pickle_path 코드 수정 - 주석 처리
+        # pickle_path = "C:/GitStudy/Python_Team_eVe/RabbitMQ-Administrator/static/all_k_shortest_paths.pickle_S_02"
         battery_csv_path = "C:/GitStudy/Python_Team_eVe/RabbitMQ-Administrator/static/"
         truck_csv_path = "C:/GitStudy/Python_Team_eVe/RabbitMQ-Administrator/static/"
 
-        eve_0522_test3.solve(datafile, pickle_path, battery_csv_path, truck_csv_path)
+        # eve_0522_test3.solve(datafile, pickle_path, battery_csv_path, truck_csv_path) # //#28 fix: pickle_path 코드 수정 - 주석 처리 
+        eve_0523_test1.solve(datafile, battery_csv_path, truck_csv_path)
 
         # Assuming there's a function in eve_0522_test.py to calculate the path and save a CSV
         # result = eve_0522_test.calculate_and_save()
